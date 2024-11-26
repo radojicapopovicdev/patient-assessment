b9ef5e64b18ade327ba62b27b7462ee8fd44ab35
Radojica Popovic <radojicapopovic@icloud.com>
2024-11-26 19:41:26 +0100

    - Convert post to assessment and fix linting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,16 +1,30 @@
 # Changelog
+
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.0.2] - 2024-11-26
 
+### Changed
+
+- Converted post.ts to assessment.ts
+- Updated root.ts to handle assessments
+
+### Fixed
+
+- Resolved ESLint warnings
 
 ## [0.0.1] - 26-11-2024
+
 ### Added
+
 - Initial database migrations
 - Schema created
 
 ## [0.0.0] - 26-11-2024
+
 ### Added
+
 - Initial commit
---
+++ src/server/api/routers/assessment.ts
@@ -0,0 +1,48 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+/**
+ * This is the assessment router of your server, which exposes two routes
+ * (functions for tRPC):
+ *
+ * - create: which lets you create a assessment by providing its details.
+ * - retrieve: which lets you retrieve all assessments:
+ */
+export const assessmentRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(
+      z.object({
+        type: z.string().trim().min(1),
+        patientName: z.string().min(1),
+        date: z.date(),
+        finalScore: z.number().min(0).max(100),
+        questions: z.array(
+          z.object({
+            question: z.string().min(1),
+            answer: z.string().min(1),
+          }),
+        ),
+      }),
+    )
+    .mutation(
+      async ({ ctx, input: { type, patientName, date, finalScore } }) => {
+        await ctx.db.assessment.create({
+          data: {
+            type,
+            patientName,
+            date,
+            finalScore,
+          },
+        });
+      },
+    ),
+
+  getAll: protectedProcedure.query(async ({ ctx }) => {
+    const assessments = await ctx.db.assessment.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+
+    return assessments;
+  }),
+});
